0936f0285569113baacf1d382340c9c8892a6542
RonAlmog <ron8132@gmail.com>
2015-08-17 00:30:50 -0400

    using router in main.js

---
+++ src/components/app.js
@@ -0,0 +1,22 @@
+// "use strict";
+// no use strict here, because we are using global vars. ($/jquery for bootstrap)
+
+var React = require('react');
+var Header = require('./common/header');
+var RouteHandler = require('react-router').RouteHandler;
+$ = jQuery = require('jquery');
+
+var App = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <Header/>
+                <div className="container-fluid">
+                    <RouteHandler/>
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = App;
